8038ba916930099d93f9c396566b70c4fd39d926
raiden808 <raiden.nalasa@gmail.com>
2020-04-21 09:34:57 +0800

    updated app js

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -3,10 +3,16 @@ import List from "./List";
 import Form from "./Form"
 
 const App = () => (
-    <div>
-        <h2>Articles</h2>
-        <List />
-    </div>
+    <>
+        <div>
+            <h2>Articles</h2>
+            <List />
+        </div>
+        <div>
+            <h2>Add a new article</h2>
+            <Form />
+        </div>
+    </>
 );
 
 export default App;
