2b353c706908aa57cdd0e0f00331785573d4d14d
raiden808 <raiden.nalasa@gmail.com>
2020-04-23 09:28:00 +0800

    added document

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,8 @@
 import store from "../js/store/index";
 import { addArticle } from "../js/actions/index";
 
+/**
+ * Connects the store to the app
+ */
 window.store = store;
 window.addArticle = addArticle;
