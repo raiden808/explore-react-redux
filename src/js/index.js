74fcb8e4bcbbaaecfc2626e130489ac6a9521d90
raiden808 <raiden.nalasa@gmail.com>
2020-04-24 09:55:30 +0800

    update react-redux

--- src/js/index.js
+++
@@ -1,8 +0,0 @@
-import store from "../js/store/index";
-import { addArticle } from "../js/actions/index";
-
-/**
- * Connects the store to the app
- */
-window.store = store;
-window.addArticle = addArticle;
