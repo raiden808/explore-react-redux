1a5d94a4680b5764524dad3937f5dfea77710fc8
raiden808 <raiden.nalasa@gmail.com>
2020-04-30 09:43:40 +0800

    middleware update

--- src/js/middleware/index.js
+++ src/js/middleware/index.js
@@ -2,31 +2,21 @@ import { ADD_ARTICLE } from "../constants/action-types";
 
 const forbiddenWords = ["spam", "money"];
 
-export function forbiddenWordsMiddleware({dispatch}){
-    return function(next){
-        return function(action){
+export function forbiddenWordsMiddleware({ dispatch }) {
+  return function(next) {
+    return function(action) {
+      // do your stuff
+      if (action.type === ADD_ARTICLE) {
+        
+        const foundWord = forbiddenWords.filter(word =>
+          action.payload.title.includes(word)
+        );
 
-            /**
-             * Only triggers if event dispatch is add_article
-             */
-            if(action.type === ADD_ARTICLE){
-                
-                /**
-                 * Check if there's a forbidden word
-                 */
-                const foundWord = forbiddenWords.filter(word => 
-                    action.payload.title.includes('word')    
-                );
-
-                /**
-                 * Triggers dispatch if error is found
-                 */
-                if(foundWord.length){
-                    return dispatch({type:"FOUND_BAD_WORD"})
-                }
-            }
-
-            return next(action);
-        };
+        if (foundWord.length) {
+          return dispatch({ type: "FOUND_BAD_WORD" });
+        }
+      }
+      return next(action);
     };
+  };
 }
